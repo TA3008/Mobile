c88c07e555a79434650443df69d2138814fd360d
TA3008 <130770406+TA3008@users.noreply.github.com>
2023-09-10 03:30:25 +0700

    Update App.js

--- App.js
+++ App.js
@@ -10,13 +10,6 @@ import ProductDetail from './screens/ProductDetail';
 import Search from './screens/Search';
 import Cart from './screens/Cart';
 
-const express = require('express');
-const app = express();
-var port = process.env.PORT || 3000;
-app.listen(port, () => console.log('Server is running on port' + port))
-const Stack = createStackNavigator();
-
-const Stack = createStackNavigator();
 
 const App = () => {
   return (
